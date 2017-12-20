1ccab531941a2bb7d883153b0773a211b35de3dd
隆成 平井 <150023@jc-21.jp>
2017-12-20 15:04:00 +0900

    変更3

--- SamplePage/SamplePage/SeekPage.xaml.cs
+++ SamplePage/SamplePage/SeekPage.xaml.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace SamplePage
+namespace BookManager
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class SeekPage : ContentPage
@@ -17,45 +17,32 @@ namespace SamplePage
             InitializeComponent();
         }
 
+        // 親カテゴリのプルダウンに応じて子カテゴリの内容を変更する
         private void OnSelectedIndexChanged(object sender, EventArgs eventArgs)
-        {/*
-            if (this.picker.SelectedIndex == 0)
+        {
+            if (this.picker2.SelectedIndex == 0)
             {
-                picker2.Items.Clear();
-                picker2.Items.Add("Baboon");
-                picker2.Items.Add("Capuchin Monkey");
-                picker2.Items.Add("Blue Monkey");
-                picker2.Items.Add("Squirrel Monkey");
-                picker2.Items.Add("Golden Lion Tamarin");
-                picker2.Items.Add("Howler Monkey");
-                picker2.Items.Add("Japanese Macaque");
+                picker3.Items.Clear();
+                picker3.Items.Add("Baboon");
+                picker3.Items.Add("Capuchin Monkey");
+                picker3.Items.Add("Blue Monkey");
+                picker3.Items.Add("Squirrel Monkey");
+                picker3.Items.Add("Golden Lion Tamarin");
+                picker3.Items.Add("Howler Monkey");
+                picker3.Items.Add("Japanese Macaque");
             }
 
-            if (this.picker.SelectedIndex == 1)
+            if (this.picker2.SelectedIndex == 1)
             {
-                picker2.Items.Clear();
-                picker2.Items.Add("2Baboon");
-                picker2.Items.Add("2Capuchin Monkey");
-                picker2.Items.Add("2Blue Monkey");
-                picker2.Items.Add("2Squirrel Monkey");
-                picker2.Items.Add("2Golden Lion Tamarin");
-                picker2.Items.Add("2Howler Monkey");
-                picker2.Items.Add("2Japanese Macaque");
+                picker3.Items.Clear();
+                picker3.Items.Add("セント");
+                picker3.Items.Add("ビント");
+                picker3.Items.Add("ティーヴィー");
+                picker3.Items.Add("Squirrel Monkey");
+                picker3.Items.Add("Golden Lion Tamarin");
+                picker3.Items.Add("Howler Monkey");
+                picker3.Items.Add("Japanese Macaque");
             }
-
-            if (this.picker.SelectedIndex == 2)
-            {
-                picker2.Items.Clear();
-                picker2.Items.Add("3Baboon");
-                picker2.Items.Add("3Capuchin Monkey");
-                picker2.Items.Add("3Blue Monkey");
-                picker2.Items.Add("3Squirrel Monkey");
-                picker2.Items.Add("3Golden Lion Tamarin");
-                picker2.Items.Add("3Howler Monkey");
-                picker2.Items.Add("3Japanese Macaque");
-            }*/
         }
     }
-
-
 }
